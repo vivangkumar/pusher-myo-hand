0eeda953b7509f19bd3e8c9f2d120d5fb8c58001
Vivan Kumar <vivangkumar@gmail.com>
2015-04-21 17:25:09 +0100

    Add session key to channel names

--- public/javascripts/game/main.js
+++ public/javascripts/game/main.js
@@ -2,12 +2,10 @@
  * Main game file.
  *
  */
+ var sessionKey = window.location.pathname.split('/')[2];
  var pusher = new Pusher(pusherKey, {authEndpoint: '/pusher/auth'});
- var handChan = pusher.subscribe('private-hand-events');
- var locChan = pusher.subscribe('private-player-locs');
-
-console.log(pusher);
-console.log(handChan);
+ var handChan = pusher.subscribe('private-hand-events-' + sessionKey);
+ var locChan = pusher.subscribe('private-player-locs-' + sessionKey);
 
  var game = new Phaser.Game(
   $(window).width(),
