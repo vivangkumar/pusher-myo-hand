f79de71c10be9d1afe12f66a5e935c86c436cfd7
Will Sewell <me@willsewell.com>
2015-04-21 12:28:57 +0100

    Hand all poses as client events.

--- public/javascripts/myo-hand.js
+++ public/javascripts/myo-hand.js
@@ -1,11 +1,14 @@
 
-var pusher = new Pusher(pusherKey);
-var handChan = pusher.subscribe('hand-events');
+var pusher = new Pusher(pusherKey, { authEndpoint: '/pusher/auth' });
+var handChan = pusher.subscribe('private-hand-events');
 
 var myMyo = Myo.create();
 
-myMyo.on('fist', function(edge){
-  if(!edge) return;
-  console.log('Hello Myo!');
-  myMyo.vibrate();
+handChan.bind('pusher:subscription_succeeded', function() {
+  myMyo.on('pose', function (poseName, edge) {
+    if (!edge) return;
+    console.log(poseName);
+    myMyo.vibrate();
+    handChan.trigger('client-pose', {'pose': poseName});
+  });
 });
