a3fc670bc23e4bac8aaf2438fa2d023d95031573
Vivan Kumar <vivangkumar@gmail.com>
2015-04-21 11:29:57 +0100

    Added pusher js

---
+++ routes/pusher.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var Pusher = require('pusher');
+
+var config = CONFIG;
+
+var pusher = new Pusher({
+  appId: config.pusherAppId,
+  key: config.pusherKey,
+  secret: config.pusherSecret
+});
+
+/** Auth endpoint for private channels **/
+router.post('/auth', function(req, res, next) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+module.exports = router;
