08149006dac5a8eeb4d4ace919ddd72f77e8714a
Vivan Kumar <vivangkumar@gmail.com>
2015-04-21 11:31:04 +0100

    Added route to pusher auth endpoint

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var index = require('./routes/index'),
+    pusher = require('./routes/pusher');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/pusher', pusher);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
