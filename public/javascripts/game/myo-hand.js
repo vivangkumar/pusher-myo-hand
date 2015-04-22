fb031d8937be0f3461d5bf0946a68a91a987e2fd
Vivan Kumar <vivangkumar@gmail.com>
2015-04-22 09:53:39 +0100

    Make myo permanently unlocked

--- public/javascripts/game/myo-hand.js
+++ public/javascripts/game/myo-hand.js
@@ -1,11 +1,15 @@
 
 var myMyo = Myo.create();
 
+// Make sure Myo does not lock continuously
+myMyo.on('connected', function() {
+  myMyo.setLockingPolicy('none');
+});
+
 handChan.bind('pusher:subscription_succeeded', function() {
   myMyo.on('pose', function (poseName, edge) {
     if (!edge) return;
     console.log(poseName);
-    myMyo.vibrate();
     handChan.trigger('client-pose', {'pose': poseName});
   });
 });
